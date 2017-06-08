513574f7c75e7bb0dc67bc1c86010c5cd7d1e8ef
Brent Kelly <brentkelly87@gmail.com>
2017-06-08 15:40:11 +0100

    added react native to sample app

--- sample-app/android/app/src/main/java/com/hudl/oss/react/sampleapp/MainActivity.java
+++ sample-app/android/app/src/main/java/com/hudl/oss/react/sampleapp/MainActivity.java
@@ -3,11 +3,30 @@ package com.hudl.oss.react.sampleapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.hudl.oss.react.fragment.ReactFragment;
+
+public class MainActivity extends AppCompatActivity implements DefaultHardwareBackBtnHandler {
+
+    private static final String COMPONENT_NAME = "SampleApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            ReactFragment reactFragment = new ReactFragment.Builder(COMPONENT_NAME).build();
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container_main, reactFragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public void invokeDefaultOnBackPressed() {
+        super.onBackPressed();
     }
 }
