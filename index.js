5ed76bd8ffa063ff3f7c97bd3bea5c87d3849a6e
John Shelley <john.shelley.p@gmail.com>
2018-04-30 10:34:55 -0500

    Setup Native Module Linker (#13)
    * Move contents to android folder, add package.json
    
    * Move sample to root
    
    * Link to example app
    
    * v1.0.0 - Update README
    
    * Update README.md

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * No-Op:
+ * 
+ * This native library handles linking the code required for use
+ * in a Native Android project, but it currently doesn't require the need to expose
+ * any bridge methods, views, or constants.
+ */
